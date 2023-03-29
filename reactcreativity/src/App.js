c8ef865b7d29e2413e1aa7b5849d81210714537a
Imad <imadakawas@gmail.com>
2023-03-29 23:50:47 +0300

    fun stuff going on

--- reactcreativity/src/App.js
+++ reactcreativity/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Icon, Card, Header } from 'semantic-ui-react';
+import { Icon, Card, Header, Container } from 'semantic-ui-react';
 import arsa from './images/elliot.jpg';
 // import CardRow1 from './CardRow';
 import './App.css';
@@ -11,7 +11,10 @@ import nav123 from './nav'
 import pag from './components/pagination'
 import acc from './components/accordian'
 import form from './components/form'
+import { Carousel } from 'bootstrap';
 // import Button from '@mui/material/Button';
+import carousel123 from './components/carousel'
+import textare123 from './components/areatext'
 
 
 const extra = (
@@ -25,64 +28,74 @@ const extra = (
 function App() {
   return (
 
-    
+
     <div className="App">
-       
+
       <div className='nav22'>
-      <Card
-          meta={nav123}></Card>
-         
-       
+        <Card
+          header={nav123}></Card>
+
+
+
+      </div>
+      <div className='card-container'>
+        <Card
+          meta={carousel123}></Card>
       </div>
 
+      <div className='Areatext'>
+        <Card
+          meta={textare123}></Card>
+      </div>
       <div className='App-header'>
-  <div className='card-container'>
-    <Card
-      image={arsa}
-      header='Elliot Baker'
-      meta={'Friend'}
-      description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
-      extra={extra}
-    />
-    <Card
-      image={arsa}
-      header='John Doe'
-      meta={'Colleague'}
-      description='John is a software developer who loves coding and playing video games in his free time.'
-      extra={extra}
-    />
-     <Card
-      image={arsa}
-      header='John Doe'
-      meta={'Colleague'}
-      description='John is a software developer who loves coding and playing video games in his free time.'
-      extra={extra}
-    />
-    
-  </div>
-</div>
-      <div className='accordian' style={{ 
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center'
-}}>
+
+        <div className='card-container'>
+          <Card
+            image={arsa}
+            header='Elliot Baker'
+            meta={'Friend'}
+            description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+            extra={extra}
+          />
+          <Card
+            image={arsa}
+            header='John Doe'
+            meta={'Colleague'}
+            description='John is a software developer who loves coding and playing video games in his free time.'
+            extra={extra}
+          />
+          <Card
+            image={arsa}
+            header='John Doe'
+            meta={'Colleague'}
+            description='John is a software developer who loves coding and playing video games in his free time.'
+            extra={extra}
+          />
+
+        </div>
+      </div>
+      <div className='accordian' style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
 
 
-<Card
+        <Card
           meta={form}></Card>
-          
+
       </div>
 
       <div className='footer'>
-      <Card
+        <Card
           meta={pag}></Card>
-         
+
         {/* <Card
           description={CardRow1}></Card> */}
 
       </div>
 
-   
+
 
     </div>
   );
--- reactcreativity/src/nav.js
+++ reactcreativity/src/nav.js
@@ -1,51 +1,50 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React, { useState } from 'react';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBNavbarBrand,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBCollapse
+} from 'mdb-react-ui-kit';
 
-function NavScrollExample() {
+export default function App() {
+  const [showNavText, setShowNavText] = useState(false);
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>React Creativity</MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          data-target='#navbarText'
+          aria-controls='navbarText'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavText(!showNavText)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar show={showNavText}>
+          <MDBNavbarNav className='mr-auto mb-5 mb-lg-0'>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Features</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+         
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
   );
 }
-
-export default NavScrollExample;
